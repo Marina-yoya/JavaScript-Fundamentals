27834c0b5421744531cb975ed90a634cef98d087
Marina Dimitrova <marina.dimitrova.yoya@gmail.com>
2021-06-08 18:22:00 +0100

    Solution added

---
+++ 03 Arrays/07_MaxSequenceOfEqualElements.js
@@ -0,0 +1,15 @@
+function foo(arr) {
+    let longestSequences = [];
+    arr.reduce((a, v) => {
+        a.push(v);
+        if (a.every((x) => x === v)) {
+            if (a.length > longestSequences.length)
+                longestSequences = a.slice();
+            return a;
+        } else {
+            return [v];
+        }
+    }, []);
+
+    return longestSequences.join(' ');
+}
