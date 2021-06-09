253a160309fac9a15a728c7742a5c9154a08209a
Marina Dimitrova <marina.dimitrova.yoya@gmail.com>
2021-06-09 14:38:41 +0100

    Solution added

---
+++ 05 Objects and Classes/01_Employees.js
@@ -0,0 +1,10 @@
+function foo(arr) {
+    const obj = arr.reduce((a, v) => {
+        a[v] = v.length;
+        return a;
+    }, {});
+
+    Object.entries(obj).forEach((x) =>
+        console.log(`Name: ${x[0]} -- Personal Number: ${x[1]}`)
+    );
+}
