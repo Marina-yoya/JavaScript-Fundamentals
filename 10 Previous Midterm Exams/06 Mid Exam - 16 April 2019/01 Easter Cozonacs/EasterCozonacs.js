ad6653ce4c30b38b01bed9f874bc503f7bab7c3a
Marina Dimitrova <marina.dimitrova.yoya@gmail.com>
2021-06-21 15:36:15 +0100

    Solution added

---
+++ 10 Previous Midterm Exams/06 Mid Exam - 16 April 2019/01 Easter Cozonacs/EasterCozonacs.js
@@ -0,0 +1,24 @@
+function foo(arr) {
+    let budget = Number(arr[0]);
+    const kiloFlourPrice = Number(arr[1]);
+    const packEggsPrice = kiloFlourPrice * 0.75;
+    const litreMilkPrice = kiloFlourPrice * 1.25;
+    const milkForOneCozonac = litreMilkPrice / 4;
+    const pricePerCozonac = kiloFlourPrice + packEggsPrice + milkForOneCozonac;
+    let cozonaces = 0;
+    let eggs = 0;
+
+    while (budget - pricePerCozonac > 0) {
+        budget -= pricePerCozonac;
+        cozonaces += 1;
+        eggs += 3;
+        if (cozonaces % 3 === 0) eggs -= cozonaces - 2;
+    }
+
+    console.log(
+        `You made ${cozonaces} cozonaces! Now you have ${eggs} eggs and ${budget.toFixed(
+            2
+        )}BGN left.`
+    );
+}
+e;
