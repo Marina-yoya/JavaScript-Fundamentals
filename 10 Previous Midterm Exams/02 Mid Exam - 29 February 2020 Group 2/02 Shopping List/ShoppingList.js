e2240dfb12231404465007f3d2ba402b8f895f9a
Marina Dimitrova <marina.dimitrova.yoya@gmail.com>
2021-06-21 15:27:54 +0100

    Solution added

---
+++ 10 Previous Midterm Exams/02 Mid Exam - 29 February 2020 Group 2/02 Shopping List/ShoppingList.js
@@ -0,0 +1,52 @@
+function foo(arr) {
+    arr = arr.filter((x) => x !== '');
+    let groceries = arr.shift().split('!');
+    const actions = {
+        Urgent: urgent,
+        Unnecessary: unnecessary,
+        Correct: correct,
+        Rearrange: rearrange,
+        Go: goShopping,
+    };
+
+    function getIndex(item) {
+        if (groceries.includes(item)) {
+            return groceries.findIndex((x) => x === item);
+        }
+    }
+
+    function urgent(item) {
+        if (!groceries.includes(item)) {
+            groceries.unshift(item);
+        }
+    }
+
+    function unnecessary(item) {
+        if (getIndex(item) !== undefined) {
+            groceries.splice(getIndex(item), 1);
+        }
+    }
+
+    function correct(oldItem, newItem) {
+        if (getIndex(oldItem) !== undefined) {
+            groceries[getIndex(oldItem)] = newItem;
+        }
+    }
+
+    function rearrange(item) {
+        if (getIndex(item) !== undefined) {
+            groceries.splice(getIndex(item), 1);
+            groceries.push(item);
+        }
+    }
+
+    function goShopping() {
+        console.log(groceries.join(', '));
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        const [action, param1, param2] = arr[i].split(' ');
+        actions[action](param1, param2);
+        if (action === 'Go') break;
+    }
+}
